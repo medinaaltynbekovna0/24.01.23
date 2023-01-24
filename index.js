68516fc23aba193bb8c5cde1aead70d3bc513d90
medina <damirovamedina38@gmail.com>
2023-01-24 15:10:34 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+let myName = "Medina";
+let myFamily = "Parent,brothers,sister";
+let myCountry = "Kyrgyzstan";
+let myHobby = "to read";
+let mySchool = "Sadyrbek Abdrakmanova";
+let myCours = "It";
+let myFavoriteAnimal = "hourse";
+let myPhone = "poco";
+let myBook = "nike";
+let mySurname = "Damirova";
+let myNumber = "five";
+let myBirthday = " January twenty-fourth ";
+let myFavoriteNumber = 5;
+let myHeight = 1.60;
+let myAdress = 108;
+
+let MoodCool = true;
+let myEnglishbad = true;
+let LiveinKyzylSuu = true;;
+
+let example1 = 20 - 5;
+let example2 = 15 + 3;
+let example3 = 4 * 3;
+let example4 = 18 / 4;
+let exaample5 = 12 / 4 * 2;
+let example6 = (8 + 2) / 5;
+let example7 = 9 / 9;
+let example8 = 6 + (2 ** 4);
+let example9  = 15 % 2;
+let example10  = 25 / 5;
+
+let example11 = 6 > 3;
+let example12 = 5 === 5;
+let example13 = 2 < 4;
+
+
